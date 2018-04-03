41f08b77da76802dd354fca3108a28ebd758c546
Conorkkkx <13520782875@163.com>
2018-04-03 16:27:29 +0800

    add 文章详情

--- blog_Django/blog/models.py
+++ blog_Django/blog/models.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.six import python_2_unicode_compatible
 
-
+@python_2_unicode_compatible
 class Category(models.Model): 
     name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible   
 class Tag(models.Model): 
     name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
 class Post(models.Model):
     # 文章标题
     title = models.CharField(max_length=100)
@@ -25,3 +37,8 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)
     # 文章作者
     author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
